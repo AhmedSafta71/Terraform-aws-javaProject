1ec8f5b61e1c1883dbe35a260d1df2bc80f96f6d
Ahmed SAFTA <ahmedsafta71@gmail.com>
2024-02-19 13:15:33 +0100

    V-20

--- templates/bean-app.tf
+++
@@ -1,8 +0,0 @@
-/*Elastic   beanstalk resource creation  */
-resource "aws_elastic_beanstalk_application" "elastic_beanstalk_app" {
-  name        = "elastic_beanstalk_app"
-  description = "elastic_beanstalk  application "
-
-}
-
-
